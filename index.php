f0c907d237cd2ca02cf4b4f4a36aa039b116617c
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-27 08:51:25 -0300

    Adicionando Bootstrap

--- index.php
+++ index.php
@@ -34,9 +34,8 @@ $pdo->exec("UPDATE pessoas SET nome='$nome' WHERE id=12") /*Deve sempre indicar 
 
     <title>Cadastro Pessoas</title>
 
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
 
-    <!-- video bootstrap -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
     <!-- video font awesome -->
 
